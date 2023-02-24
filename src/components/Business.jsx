74166899e98b99ef20375a2f782f555cde66ca8e
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-24 10:37:55 -0300

    stats

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { features } from "../constants/index.js";
+import styles, { layout } from "../style.js";
+import Button from "./Button.jsx";
 
-const Business = () => {
-  return <div></div>;
-};
+const Business = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="hidden sm:block" /> we'll handle the
+        money.
+      </h2>
+      <p>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds of
+        credit cards on the market.
+      </p>
+    </div>
+  </section>
+);
 
 export default Business;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { stats } from "../constants";
+import styles from "../style.js";
 
-const Stats = () => {
-  return <div></div>;
-};
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex´wrap mb-6 flex-row sm:mb-20`}>
+    {stats.map((stat) => (
+      <div
+        key={stat.id}
+        className={`m-3 flex flex-1 flex-row items-center justify-start`}
+      >
+        <h4 className="xs:text=[40px] font-poppins text-[30px] font-semibold leading-[43px] text-white xs:leading-[53px]">
+          {stat.value}
+        </h4>
+        <p className="xs:text=[20px] text-gradient ml-3 font-poppins text-[15px] font-normal uppercase leading-[21px] xs:leading-[26px]">
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
 export default Stats;
