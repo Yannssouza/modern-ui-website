f05ab7a5004678469bf50b1ecb1513ebf8ef7328
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2023-02-25 09:47:36 -0300

    cardDeal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`text-primary outline-none ${styles} bg-blue-gradient py-4 px-6 font-poppins text-[18px] font-medium`}
+    className={`text-primary outline-none ${styles} bg-blue-gradient rounded-[10px] py-4 px-6 font-poppins text-[18px] font-medium`}
   >
     Get Started
   </button>
